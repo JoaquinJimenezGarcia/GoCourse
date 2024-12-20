dcb87467db74a076245f49c021c06a2656ea05ab
Joaquin Jimenez Garcia <garcia.jjimenez@gmail.com>
2024-12-20 23:55:03 +0100

    Changed comments

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,7 @@
-package main # By setting "main" we are telling Go that this is an executable program
+package main // By setting "main" we are telling Go that this is an executable program
 
-import "fmt" # Importing the fmt package to use the Println function
+import "fmt" // Importing the fmt package to use the Println function
 
-func main() { # The main function is the entry point of the program
+func main() { // The main function is the entry point of the program
 	fmt.Println("Hello World!")
 }
