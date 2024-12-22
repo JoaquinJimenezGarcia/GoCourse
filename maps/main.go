39afa13822b84fc6f3ac1bada67543560057c35c
Joaquín Jiménez García <joaquin.garcia@wpengine.com>
2024-12-22 18:00:42 +0100

    more about maps

--- maps/main.go
+++ maps/main.go
@@ -9,4 +9,16 @@ func main() {
 		"white": "#000000",
 		"green": "#4fg567",
 	}
+
+	var otherColors map[string]string
+
+	fmt.Println(colors)
+	fmt.Println(otherColors) // Initialized with zero-values
+
+	nonZeroValues := make(map[string]string) // It's empty, not zero-values
+
+	fmt.Println(nonZeroValues)
+
+	delete(colors, "red") // Deleteing an entry
+	fmt.Println(colors)
 }
