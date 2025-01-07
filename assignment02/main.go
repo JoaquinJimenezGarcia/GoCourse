ca2ced640ee1efa7e1511f5d60071d4e3d93805c
Joaquin Jimenez Garcia <joaquinjimenez_garcia@genpt.com>
2025-01-07 12:07:11 +0100

    Added assignment 02

---
+++ assignment02/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type logWriter struct{}
+
+func main() {
+	filename := os.Args[1]
+
+	fmt.Println("File name:", filename)
+
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lw := logWriter{}
+
+	io.Copy(lw, file)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	return len(bs), nil
+}
